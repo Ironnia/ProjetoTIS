d110a129d6d7882bde7e782874beec4e3fd26a85
ironnia <mrironnia@gmail.com>
2026-05-06 14:57:53 -0300

    calculo dos dias para usar em validade e mudanças de tipos

--- etl/src/main/kotlin/etl/Transformer.kt
+++ etl/src/main/kotlin/etl/Transformer.kt
@@ -1,5 +1,9 @@
 package etl
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 class DataTransformer {
 
     // Valida se o mapa tem as colunas obrigatórias. Vai retornar nada se estiver tudo certo.
@@ -11,5 +15,21 @@ class DataTransformer {
         }
 
         return null
+    }
+
+    // Trtar a vírgula como ponto
+    fun parseDouble(value: String): Double = value.replace(",", ".").toDoubleOrNull() ?: 0.0
+
+    fun parseInt(value: String): Int = value.toIntOrNull() ?: 0
+
+    fun calculateDaysToExpire(dateStr: String): Int {
+        return try {
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val expiration = LocalDate.parse(dateStr, formatter)
+            val today = LocalDate.now()
+            ChronoUnit.DAYS.between(today, expiration).toInt()
+        } catch (e: Exception) {
+            -1 // Data inválida
+        }
     }
 }
